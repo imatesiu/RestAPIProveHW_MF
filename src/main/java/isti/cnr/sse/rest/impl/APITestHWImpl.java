70f4bea226beedead6ae79bacff611487993f0a9
Giorgio <imatesiu@gmail.com>
2016-12-04 14:52:18 +0100

    save attach added

--- src/main/java/isti/cnr/sse/rest/impl/APITestHWImpl.java
+++ src/main/java/isti/cnr/sse/rest/impl/APITestHWImpl.java
@@ -1,6 +1,9 @@
 package isti.cnr.sse.rest.impl;
 
 import java.util.Map;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -20,11 +23,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.jboss.weld.environment.util.Collections;
+import org.apache.commons.codec.binary.Base64;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import isti.cnr.sse.rest.data.Allegato;
 import isti.cnr.sse.rest.data.Ditta;
 import isti.cnr.sse.rest.data.Ditte;
 import isti.cnr.sse.rest.data.Factory;
@@ -34,7 +38,7 @@ import isti.cnr.sse.rest.data.Prova;
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/cnr/sse/testhw")
 public class APITestHWImpl{
-	
+
 	//@Inject 
 	//TokenPersistence em;
 
@@ -43,15 +47,15 @@ public class APITestHWImpl{
 	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(APITestHWImpl.class);
 
 
-	
 
-/*	@Path("/")
+
+	/*	@Path("/")
 	@POST
 	public String putListMisuratoriFiscale(@FormParam("sendmisuratori") MisuratoriFiscale misuratori){
 		return "OK";
-		
+
 	}*/
-	
+
 	@Path("/ditta/")
 	@POST
 	public String putDitta(String  d){
@@ -64,9 +68,9 @@ public class APITestHWImpl{
 		}
 		map.put(ditta.getNomeDitta(), ditta);
 		return "OK";
-		
+
 	}
-	
+
 	@Path("/modello/")
 	@POST
 	public String putModello(String  MF){
@@ -81,37 +85,88 @@ public class APITestHWImpl{
 			return "Modello gia presente";
 		}
 		return "Ditta non trovata";
-		
+
 	}
-	
+
 	@Path("/provaupdated/")
 	@POST
 	public String putProveHW(String  Prova){
 		//Type listType = new TypeToken<ArrayList<Prova>>(){}.getType();
 
-		
+
 		Gson g = new Gson();
 		//List<Prova> Listprove = g.fromJson(MF, listType);
 		Prova prova = g.fromJson(Prova, Prova.class);
-		
-		
+		String d = prova.getNomeDitta();
+		if(map.containsKey(d)){
+			Ditta ditta = map.get(d);
+			int index = ditta.getMisuratoriFiscali().indexOf(new ModelloMF(prova.getNomeModello(),prova.getNumeroRapportoProva() ,prova.getNomeDitta()));
+			if(index>=0){
+				ModelloMF modello = ditta.getMisuratoriFiscali().get(index);
+				int ind = modello.getProve().indexOf(prova);
+				if(ind>=0){
+					Prova pp = modello.getProve().get(ind);
+					//TODO: cambiare dove salvare i file 
+					saveallegati(prova.getListallegato());
+					pp.getListallegato().addAll(prova.getListallegato());
+
+				}
+			}
+		}
+
 		return "OK";
-		
+
 	}
-	
-	
+
+
+	private void saveallegati(List<Allegato> listallegato) {
+		for (Allegato allegato : listallegato) {
+			String nome = allegato.getNome();
+			String num = allegato.getNumeroRapportoProva();
+			String datibase64 = allegato.getDati();
+			allegato.setDati("");
+			saveFile(nome,num, datibase64);
+			allegato.setUrl("http://localhost:9090/cnr/sse/testhw/allegati/"+num+"/"+nome);
+		}
+
+	}
+
+	private void saveFile(String nome, String num, String datibase64) {
+		// TODO Auto-generated method stub
+		try{
+			File theDir = new File("allegati/"+num);
+
+			// if the directory does not exist, create it
+			if (!theDir.exists()) {
+				theDir.mkdir();
+			}
+
+
+			byte[] imageByteArray = Base64.decodeBase64(datibase64);
+
+			FileOutputStream imageOutFile = new FileOutputStream("allegati/"+num+"/"+nome);
+			imageOutFile.write(imageByteArray);
+
+			imageOutFile.close();
+
+		} catch (IOException ioe) {
+			System.out.println("Exception while reading the Image " + ioe);
+		}
+
+	}
+
 	@Path("/dittestring/")
 	@GET
 	public String getDitteName(){
-		
-		
+
+
 		Collection<String> d = map.keySet();
 		Gson g = new Gson();
 		return  g.toJson(d);
-		
-		
+
+
 	}
-	
+
 
 	@Path("/ditte/{howmany:.*}")
 	@GET
@@ -122,50 +177,50 @@ public class APITestHWImpl{
 				MFS.setMap(map);
 				return  MFS;*/
 			}
-		//	if(numeroelementi.equals("test")){			
-				
-				
-				Ditte d = new Ditte();
-				d.setListaDitte(new ArrayList<>(map.values()));
-				Gson g = new Gson();
-				return  g.toJson(d);
-		//	}
-		//	return null;
+			//	if(numeroelementi.equals("test")){			
+
+
+			Ditte d = new Ditte();
+			d.setListaDitte(new ArrayList<>(map.values()));
+			Gson g = new Gson();
+			return  g.toJson(d);
+			//	}
+			//	return null;
 		}catch(Exception e){
 			log.fatal("Fatal "+e.getMessage());
 			return null;
 		}
-		
-		
+
+
 	}
-	
+
 	@Path("/allegati/{id:.*}/{nome:.*}")
 	@GET
 	public Response getAllegato(@PathParam("id") String id, @PathParam("nome") String nome){
-		
+
 		InputStream is = APITestHWImpl.class.getClassLoader().getResourceAsStream("mini.jpg");
-		
-		
+
+
 		return Response
-                .ok(is, MediaType.APPLICATION_OCTET_STREAM)
-                .header("content-disposition","attachment; filename = "+nome)
-                .build();
-		
-		
+				.ok(is, MediaType.APPLICATION_OCTET_STREAM)
+				.header("content-disposition","attachment; filename = "+nome)
+				.build();
+
+
 	}
-	
-/*	@Path("/misuratori2/{howmany:.*}")
+
+	/*	@Path("/misuratori2/{howmany:.*}")
 	@GET
 	public String getListMisuratoriFiscale2(@PathParam("howmany") String numeroelementi){
 		try{
-			
+
 			if(numeroelementi.equals("test")){			
 				JsonFactory factory = new JsonFactory();
-				
+
 				MisuratoriFiscale LMF = factory.getMisuratoriFiscale();
 				Gson gson = new Gson();
-				 
-				
+
+
 				return  gson.toJson(LMF);
 			}
 			return null;
@@ -173,10 +228,10 @@ public class APITestHWImpl{
 			log.fatal("Fatal "+e.getMessage());
 			return null;
 		}
-		
-		
+
+
 	}
-	
+
 	@Path("/misuratore/{matricola:.*}")
 	@GET
 	public MisuratoreFiscale getStatusCollaborativeContentVerifications(@PathParam("matricola") String matricola){
@@ -189,7 +244,7 @@ public class APITestHWImpl{
 			log.fatal("Fatal "+e.getMessage());
 			return null;
 		}
-		
+
 	}*/
 
 
